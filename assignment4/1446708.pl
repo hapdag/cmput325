e56e4952b7010d2e8928d245140a79fada2c7582
hapdag <syhe@ualberta.ca>
2017-04-06 12:42:05 -0700

    added sendmoney puzzle clpfd example.

--- assignment4/1446708.pl
+++ assignment4/1446708.pl
@@ -1,4 +1,8 @@
 :- use_module(library(clpfd)). 
-fourSquares(+N, [-S1,-S2,-S3,-S4]):-
-	vars = [S1,S2,S3,S4],
-	vars ins 0..9.
+fourSquares(N, [S1,S2,S3,S4]):-
+	Vars = [S1,S2,S3,S4],
+	Vars ins 0..9,
+	all_different(Vars),
+	N #= -S1 * -S1.
+
+	%-S1 #=< -S2 #=< -S3 #=< -S4.
